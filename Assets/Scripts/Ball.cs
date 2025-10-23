aa2b32020822ca8a5b238241c04c73a261940ff3
Denis Semenchuk <deniss_brest@mail.ru>
2025-10-23 13:15:30 +0300

    upgrade ball and camera movement

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -3,45 +3,58 @@ using UnityEngine;
 [SelectionBase]
 public class Ball : MonoBehaviour
 {
-    [SerializeField] private GroundChecker _groundChecker;
+    private const string HorizontalAxis = "Horizontal";
+    private const string VerticalAxis = "Vertical";
 
+    [Header("Objects")]
+    [SerializeField] private GroundChecker _groundChecker;
+    [SerializeField] private Transform _camera;
+
+    [Header("Movement Settings")]
     [SerializeField] private float _speed;
     [SerializeField] private float _force;
     [SerializeField] private float _jumpForce;
 
-    private Rigidbody _rigitbody;
-
+    private Rigidbody _rigidbody;
     private bool _isJump;
 
     private void Awake()
     {
-        _rigitbody = GetComponent<Rigidbody>();
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && _groundChecker.IsGround)
             _isJump = true;
     }
 
     private void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W))
-            _rigitbody.AddForce(Vector3.forward * _speed * _force, ForceMode.Force);
+        Vector3 moveDirection = GetCameraDirection();
 
-        if (Input.GetKey(KeyCode.S))
-            _rigitbody.AddForce(Vector3.back * _speed * _force, ForceMode.Force);
+        _rigidbody.AddForce(moveDirection * _speed * _force);
 
-        if (Input.GetKey(KeyCode.D))
-            _rigitbody.AddForce(Vector3.right * _speed * _force, ForceMode.Force);
-
-        if (Input.GetKey(KeyCode.A))
-            _rigitbody.AddForce(Vector3.left * _speed * _force, ForceMode.Force);
-
-        if (_isJump && _groundChecker.IsGround)
+        if (_isJump)
         {
-            _rigitbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+            _rigidbody.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
             _isJump = false;
         }
+    }
+
+    private Vector3 GetCameraDirection()
+    {
+        float horizontal = Input.GetAxis(HorizontalAxis);
+        float vertical = Input.GetAxis(VerticalAxis);
+
+        Vector3 cameraForward = _camera.forward;
+        cameraForward.y = 0f;
+        cameraForward.Normalize();
+
+        Vector3 cameraRight = _camera.right;
+        cameraRight.y = 0f;
+        cameraRight.Normalize();
+
+        return (cameraForward * vertical + cameraRight * horizontal).normalized;
     }
 }
--- Assets/Scripts/FollowToTarget.cs
+++ Assets/Scripts/FollowToTarget.cs
@@ -3,11 +3,26 @@ using UnityEngine;
 public class FollowToTarget : MonoBehaviour
 {
     [SerializeField] private Transform _target;
-
     [SerializeField] private Vector3 _offset;
+    [SerializeField] private float _sensitivity = 200f;
 
-    private void LateUpdate()
+    private float _rotationAngleY;
+
+    private void Start()
     {
-        transform.position = _target.position + _offset;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * _sensitivity * Time.deltaTime;
+        _rotationAngleY += mouseX;
+
+        Quaternion rotation = Quaternion.Euler(0f, _rotationAngleY, 0f);
+        Vector3 desiredPosition = _target.position + rotation * _offset;
+
+        transform.position = desiredPosition;
+        transform.LookAt(_target);
     }
 }
